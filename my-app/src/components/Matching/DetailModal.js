7dfae2e23b62adbc3f9030e0c997b906fd9dc92f
Sohyun Kim <prrsh22@gmail.com>
2021-06-19 08:25:17 +0900

    [FIX] Fix bug

--- my-app/src/components/Matching/DetailModal.js
+++ my-app/src/components/Matching/DetailModal.js
@@ -1,14 +1,16 @@
-import { Button } from "@material-ui/core";
+import { Button, LinearProgress } from "@material-ui/core";
 import { useState } from "react";
 import Chat from "../Chat/Chat";
 import Loader from "../Loader";
 
 const DetailModal = ({open}) => {
-    const [isLoading, setLoadingState] = useState(true);
+    const [isLoading, setLoadingState] = useState(false);
     const [userData, setUserData] = useState(null);
     const [isChatting, setIsChatting] = useState(false);
 
-    if (open && !userData) {
+    if (!isLoading && open && !userData) {
+        setLoadingState(true);
+        console.log('run');
         //API로 추가적 정보 로드
         setTimeout(() => {
             setUserData({
@@ -20,13 +22,13 @@ const DetailModal = ({open}) => {
         }, 3000);
     }
 
-    if (!open) setIsChatting(false);
+    if (!open && isChatting) setIsChatting(false);
 
-    if (isLoading) return <Loader message="사용자 정보를 로드하는 중입니다.." />
+    if (isLoading) return <div>사용자 데이터를 불러오는 중입니다 .. <LinearProgress /></div>
     else if (isChatting) return <Chat />
     else {
         return <div id="card-modal">
-        기본 데이터 + {userData.detail}
+        기본 데이터 + {userData?.detail}
         <Button onClick={() => setIsChatting(true)}>Start Chat</Button>
         </div>
     }
