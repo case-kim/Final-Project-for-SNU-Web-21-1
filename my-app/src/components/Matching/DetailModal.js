aed7c48cce3b1d5e6b48c71f1f31e5524e385595
Sohyun Kim <prrsh22@gmail.com>
2021-06-20 14:55:16 +0900

    [STYLE] Style DetailModal

--- my-app/src/components/Matching/DetailModal.js
+++ my-app/src/components/Matching/DetailModal.js
@@ -1,3 +1,4 @@
+import { Grid } from "@material-ui/core";
 import { Button, LinearProgress } from "@material-ui/core";
 import { useState } from "react";
 import Chat from "../Chat/Chat";
@@ -15,7 +16,8 @@ const DetailModal = ({open}) => {
             setUserData({
                 username: '***',
                 type: 'A',
-                detail: '추가적으로 끌어올 데이터'
+                detail: '추가적으로 끌어올 데이터',
+                photo: 'https://mblogthumb-phinf.pstatic.net/20161004_87/by267_1475545735829x3bvR_JPEG/%BC%AD%BF%EF%B4%EB%C7%D0%B1%B3%B7%CE%B0%ED-06.jpg?type=w800'
             })
             setLoadingState(false);
         }, 3000);
@@ -23,15 +25,28 @@ const DetailModal = ({open}) => {
 
     if (!open && isChatting) setIsChatting(false);
 
-    return <div className="card-modal">
+    return <div className="card-modal center">
         {isLoading && <div>사용자 데이터를 불러오는 중입니다 .. <LinearProgress /></div>}
         
         {isChatting && <Chat />}
 
-        {!isLoading && !isChatting && <div>
-                기본 데이터 + {userData?.detail}
-                <Button onClick={() => setIsChatting(true)}>Start Chat</Button>
-            </div>}
+        {!isLoading && !isChatting && 
+            <Grid container>
+
+                <Grid item xs={6} className="center">
+                    <img className="modal-img" src={userData?.photo} />
+                    <h2>{userData?.username}</h2>
+                    <h2>{userData?.type}</h2>
+                </Grid>
+
+                <Grid item xs={6} className="center">
+                    <h1>"한마디"</h1>
+                    <ul>
+                        {[1,2,3,4].map(num => <li>설명{num}입니다. 설명{num} 블라블라</li>)}
+                    </ul>
+                    <Button onClick={() => setIsChatting(true)}>Start Chat</Button>
+                </Grid>
+            </Grid>}
 
     </div>
 }
