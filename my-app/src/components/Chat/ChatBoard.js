c47d4887a9c140cafb5ba67de163b232bf778eeb
Sohyun Kim <prrsh22@gmail.com>
2021-06-22 15:56:13 +0900

    [FEAT] Restructure loadList. Display counterName, last message(createdAt, from) on the list

--- my-app/src/components/Chat/ChatBoard.js
+++ my-app/src/components/Chat/ChatBoard.js
@@ -1,18 +1,57 @@
 import { Button, Grid } from "@material-ui/core";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "../../firebase";
 import Chat from "./Chat";
 import ChatList from "./ChatList";
+import { firestore } from '../../firebase';
+import Loader from "../Loader";
 
 const ChatBoard = () => {
 
     const user = auth.currentUser;
+
+    const [isLoading, setLoadingState] = useState(true);
+
     const [currentCounterId, setCurrentCounterId] = useState(null);
 
+    const [chatList, setChatList] = useState([]);
+
+    const loadList = () => {
+        //그냥 채팅 전체를 추적하고 있어야 새 챗도 볼 수 있음
+        firestore.collection("chatRooms").onSnapshot(async(snapshot) => {
+            let userRooms = [];
+            snapshot.docs.forEach(room => {
+                if (room.data().participants.includes(user.uid)) userRooms.push(room.data());
+            });
+
+            userRooms = userRooms.sort((a,b) => b.lastMessage.createdAt - a.lastMessage.createdAt).map(async(room) => {
+                const counterName = await getCounterName(room);
+                return {...room, counterName}
+            });
+
+            const userRoomsWithCounterName = await Promise.all(userRooms);
+
+            setChatList([...userRoomsWithCounterName]);
+            setLoadingState(false);
+        });
+    }
+
+    const getCounterName = async (room) => {
+        const counterDoc = firestore.collection("users").doc(room.participants.find(p => p !== user.uid));
+        const counter = await counterDoc.get();
+        return counter.data().username;
+    }
+
+    useEffect(() => {
+        loadList();
+    }, []);
+
+
+    if (isLoading) return <Loader message="채팅 목록을 로드하는 중입니다.."/>
     return <Grid container>
 
     <Grid item xs={4} className="center">
-        <ChatList userId={user.uid} setCurrentCounterId={setCurrentCounterId} />
+        <ChatList userId={user.uid} setCurrentCounterId={setCurrentCounterId} chatList={chatList} />
     </Grid>
 
     <Grid item xs={8} className="center">
