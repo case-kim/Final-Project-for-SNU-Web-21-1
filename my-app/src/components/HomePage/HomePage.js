eb0283005f84a6173161465ce9be044c05370c92
case-kim <kwkim9037@gmail.com>
2021-06-23 06:06:52 +0900

    Update Homepage
    DB 유무에 따라 뜨는 버튼이 달라지게끔 코딩.

--- my-app/src/components/HomePage/HomePage.js
+++ my-app/src/components/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, useEffect} from "react";
 import {Box, Button, ButtonGroup} from '@material-ui/core'
 
 import PropTypes from "prop-types";
@@ -27,6 +27,7 @@ import './style-hompage.css';
 
 
 class HomePage extends Component {
+
     signInWithEmailLink = () => {
         const {user} = this.props;
 
@@ -91,7 +92,41 @@ class HomePage extends Component {
             uid = currentUser.uid
         }
         const myDB = firebase.database().ref(`accounts/${uid}`)
-
+        myDB.once('value')
+            .then(function(snapshot){
+                if(!document.getElementById('testButton') && authentication.getProfileCompletion({...user}) >= 80&&!snapshot.hasChildren()) {
+                    const testButton = document.createElement('button')
+                    const test = document.createElement('a')
+                    testButton.appendChild(test);
+                    testButton.id = 'testButton'
+                    test.href = './test'
+                    test.innerHTML = 'TEST START'
+                    document.getElementById('buttonGroup').appendChild(testButton)
+                }
+                if (!document.getElementById('resultButton') && user && authentication.getProfileCompletion({...user}) >= 80&&snapshot.hasChildren()){
+                    const resultButton = document.createElement('button')
+                    const matchButton = document.createElement('button')
+                    const chatButton = document.createElement('button')
+                    const result = document.createElement('a')
+                    const match = document.createElement('a')
+                    const chat = document.createElement('a')
+                    resultButton.id = 'resultButton'
+                    matchButton.id = 'resultButton'
+                    chatButton.id = 'resultButton'
+                    resultButton.appendChild(result);
+                    matchButton.appendChild(match);
+                    chatButton.appendChild(chat);
+                    result.href = './result'
+                    match.href = './match'
+                    chat.href = './chatting'
+                    result.innerHTML = 'MY RESULT'
+                    match.innerHTML = 'MATCHING'
+                    chat.innerHTML = 'CHATTING'
+                    document.getElementById('buttonGroup').appendChild(resultButton)
+                    document.getElementById('buttonGroup').appendChild(matchButton)
+                    document.getElementById('buttonGroup').appendChild(chatButton)
+                }
+            })
 
         if (user && authentication.getProfileCompletion({...user}) >= 80) {
             return (
@@ -101,16 +136,12 @@ class HomePage extends Component {
                     top="50%"
                     left="50%"
                     textAlign="center">
-                    <div className="homepage-subtitle">당신과 맞는 성향의 사람을 찾아보세요.</div>
+                    <div className="homepage-subtitle">당신과 맞는 사람을 찾아보세요.</div>
                     <div className="homepage-title">SNU Flower Matching</div>
-                    <ButtonGroup variant='contained' display='flex' flex-direction='column'>
-                        <Button href='./test'>Test Start</Button>
-                        <Button href='./result'>My Result</Button>
-                        <Button href='./matching'> Matching </Button>
-                        <Button href="./chatting"> Chatting </Button>
+                    <ButtonGroup id = 'buttonGroup' color = 'primary' variant='contained' display='flex' flex-direction='column'>
                     </ButtonGroup>
                 </Box>
-            );
+            )
         }
         if (user && authentication.getProfileCompletion({...user}) < 80) {
             return (
@@ -120,7 +151,7 @@ class HomePage extends Component {
                     top="50%"
                     left="50%"
                     textAlign="center">
-                    <div>당신의 정보를 Setting에서 설정해주세요. 메일을 제외하고 사진과 모든 정보를 채워야 테스트가 가능합니다.</div>
+                    <div className="homepage-subtitle">당신의 정보를 Setting에서 설정해주세요.<br/>메일을 제외하고 사진과 모든 정보를 채워야 테스트가 가능합니다.</div>
                     <Button onClick={() => SettingsDialog}> Setup </Button>
 
                 </Box>
@@ -128,11 +159,16 @@ class HomePage extends Component {
         }
 
         return (
-            <EmptyState
-                image={<InsertBlockIllustration/>}
-                button="RMUIF"
-                description="Supercharged version of Create React App with all the bells and whistles."
-            />
+            <Box
+                style={{transform: "translate(-50%, -50%)"}}
+                position="absolute"
+                top="50%"
+                left="50%"
+                textAlign="center">
+                <div>꽃으로 알아보는 운명적인 만남</div>
+                <div className="homepage-title">SNU Flower Matching</div>
+                <div> 회원가입 및 로그인 후에 사용해주세요</div>
+            </Box>
         );
     }
 
