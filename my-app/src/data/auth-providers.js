5f6987a19316fb42a717c8ab3f709c9fbfe062dd
case-kim <kwkim9037@gmail.com>
2021-06-23 16:30:42 +0900

    Update auth-providers.js
    안 쓰는 버튼 삭제

--- my-app/src/data/auth-providers.js
+++ my-app/src/data/auth-providers.js
@@ -9,18 +9,7 @@ import { Twitter as TwitterIcon } from "mdi-material-ui";
 import { Yahoo as YahooIcon } from "mdi-material-ui";
 
 const authProviders = [
-  {
-    id: "apple.com",
-    color: "#000000",
-    icon: <AppleIcon />,
-    name: "Apple",
-  },
-  {
-    id: "facebook.com",
-    color: "#3c5a99",
-    icon: <FacebookIcon />,
-    name: "Facebook",
-  },
+
   {
     id: "github.com",
     color: "#24292e",
@@ -34,24 +23,7 @@ const authProviders = [
     icon: <GoogleIcon />,
     name: "Google",
   },
-  {
-    id: "microsoft.com",
-    color: "#0078d7",
-    icon: <MicrosoftIcon />,
-    name: "Microsoft",
-  },
-  {
-    id: "twitter.com",
-    color: "#1da1f2",
-    icon: <TwitterIcon />,
-    name: "Twitter",
-  },
-  {
-    id: "yahoo.com",
-    color: "#410093",
-    icon: <YahooIcon />,
-    name: "Yahoo",
-  },
+
 ];
 
 export default authProviders;
